fc4377600d6ab0d2f2ed5a4a4828b18019fd0542
JP <jpcodingjourney@gmail.com>
2023-04-03 00:31:31 +1000

    Add carousel

--- src/components/GameCarousel.js
+++ src/components/GameCarousel.js
@@ -1,21 +1,24 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import Carousel from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import styles from "./GameCarousel.module.css";
+import Head from "next/head";
 
 function GameCarousel() {
   const games = [
     {
-      name: "Game 1",
+      name: "Genshin Impact",
       imageUrl: "/genshin1.jpg",
       description: "Description of Game 1",
     },
     {
-      name: "Game 2",
+      name: "Path of Exile",
       imageUrl: "/genshin1.jpg",
       description: "Description of Game 2",
     },
     {
-      name: "Game 3",
+      name: "Undertale",
       imageUrl: "/genshin1.jpg",
       description: "Description of Game 3",
     },
@@ -27,18 +30,24 @@ function GameCarousel() {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    arrows: true,
   };
 
   return (
-    <Carousel {...settings}>
-      {games.map((game) => (
-        <div key={game.name}>
-          <h3>{game.name}</h3>
-          <img src={game.imageUrl} alt={game.name} />
-          <p>{game.description}</p>
-        </div>
-      ))}
-    </Carousel>
+    <div className={styles.carouselContainer}>
+      <Head>
+        <link rel="stylesheet" type="text/css" href="/slick-theme.css" />
+      </Head>
+      <Carousel {...settings}>
+        {games.map((game) => (
+          <div key={game.name}>
+            <h3>{game.name}</h3>
+            <img src={game.imageUrl} alt={game.name} />
+            <p>{game.description}</p>
+          </div>
+        ))}
+      </Carousel>
+    </div>
   );
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -21,27 +21,13 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+      {/* <main className={styles.main}>
         <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <div className={styles.carousel}>
-            <GameCarousel />
-          </div>
+          <div className={styles.carousel}> */}
+      <GameCarousel />
+      {/* </div>
         </div>
-      </main>
+      </main> */}
     </>
   );
 }
