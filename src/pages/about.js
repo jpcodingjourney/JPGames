56eccdbf34ae6157a9b11438d87e1b45dd8e0838
JP <jpcodingjourney@gmail.com>
2023-04-11 23:51:39 +1000

    Add about us statement

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,18 +11,18 @@ const About = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-        <div>about page about page about page about page about page</div>
-        <div>about page about page about page about page about page</div>
-        <div>about page about page about page about page about page</div>
-        <div>about page about page about page about page about page</div>
-        <div>about page about page about page about page about page</div>
-        <div>about page about page about page about page about page</div>
-        <div>about page about page about page about page about page</div>
-        <div>about page about page about page about page about page</div>
-        <div>about page about page about page about page about page</div>
-        <div>about page about page about page about page about page</div>
-        <div>about page about page about page about page about page</div>
-        <div>about page about page about page about page about page</div>
+        <div>
+          Welcome to my personal game website, where I share a collection of
+          games that I love to play. As a passionate gamer, I have a particular
+          affinity for role-playing games (RPGs) and massively multiplayer
+          online games (MMOs). I have curated a selection of my favorite titles
+          in these genres, ranging from classic games to modern releases. My
+          hope is to provide a platform for fellow gamers to discover and enjoy
+          new games, as well as revisit old favorites. Whether you're an RPG
+          enthusiast, an MMO veteran, or simply someone who loves to game, I
+          invite you to explore the games on my website and join me in the
+          wonderful world of gaming.
+        </div>
       </div>
     </>
   );
