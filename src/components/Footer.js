011c0fe334ac84ff82963235c1a5104f13371683
JP <jpcodingjourney@gmail.com>
2023-04-13 23:51:05 +1000

    Add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+// import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="flex justify-center p-10 dark:text-white">
+      © 2023 James Pan
+    </footer>
+  );
+};
+
+export default Footer;
