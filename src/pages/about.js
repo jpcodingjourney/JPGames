ce6f36227a59f235a46878074127f3c3c3742b4e
JP <jpcodingjourney@gmail.com>
2023-05-02 23:56:25 +1000

    Responsive updates v2

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,9 +11,9 @@ const About = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="flex pt-32">
-        <div>
-          <div className="px-96 text-xl mb-16">
+      <div className="flex pt-8 lg:pt-12 2xl:pt-20">
+        <div className="px-16">
+          <div className="lg:px-96 lg:text-xl mb-16">
             Welcome to my personal game website, where I share a collection of
             games that I love to play. As a passionate gamer, I have a
             particular affinity for role-playing games (RPGs) and massively
@@ -21,7 +21,7 @@ const About = () => {
             favorite titles in these genres, ranging from classic games to
             modern releases.
           </div>
-          <div className="px-96 text-xl mb-10">
+          <div className="lg:px-96 lg:text-xl mb-10">
             My hope is to provide a platform for fellow gamers to discover and
             enjoy new games, as well as revisit old favorites. Whether you're an
             RPG enthusiast, an MMO veteran, or simply someone who loves to game,
