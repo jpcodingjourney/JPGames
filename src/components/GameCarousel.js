6641ba1bbfa422b79e5cbeba578b90cb69c90fcd
JP <jpcodingjourney@gmail.com>
2023-05-25 23:54:38 +1000

    Font size updates

--- src/components/GameCarousel.js
+++ src/components/GameCarousel.js
@@ -90,7 +90,7 @@ function GameCarousel() {
         {games.map((game) => (
           <div className="game-container 2xl:px-12">
             <div key={game.name} className="px-2 md:px-4 lg:px-8 2xl:px-0 ">
-              <h3 className="text-center mb-5 2xl:my-5 text-3xl font-medium">
+              <h3 className="text-center mb-5 2xl:my-5 lg:text-2xl 2xl:text-3xl font-medium">
                 {game.name}
               </h3>
               <a href={`#${game.name}`}>
@@ -100,7 +100,7 @@ function GameCarousel() {
                   className="mx-auto transform hover:brightness-105 hover:scale-105 transition-all duration-300"
                 />
               </a>
-              <p className="text-justify tracking-wide my-5 2xl:text-xl">
+              <p className="text-justify tracking-wide my-5 lg:text-sm 2xl:text-xl">
                 {game.description}
               </p>
             </div>
